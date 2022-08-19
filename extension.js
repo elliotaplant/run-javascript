6e4e0f3a8add150dbcc20fce9ab06b40cd5f0639
Elliot Plant <elliotplant@gmail.com>
2022-08-19 12:25:22 -0700

    Add command palette

--- extension.js
+++ extension.js
@@ -6,14 +6,37 @@ const commands = require('/Users/elliot/.vscode-run-javascript/index.js');
  */
 function activate(context) {
   console.log('Registering commands for "run-javascript"');
-  Object.entries(commands).forEach(([commandName, commandFunction]) => {
+
+  // Register commands for all of the methods in the commands directory
+  Object.entries(commands).forEach(([functionName, commandFunction]) => {
+    const commandName = `run-javascript.${functionName}`;
     console.log('Registering command', commandName);
-    const disposable = vscode.commands.registerCommand(
-      `run-javascript.${commandName}`,
-      () => commandFunction(vscode, context)
+    const disposable = vscode.commands.registerCommand(commandName, (...args) =>
+      commandFunction(vscode, context, ...args)
     );
     context.subscriptions.push(disposable);
   });
+
+  // Register the "run-script" command that can be accessed from the command palette
+  console.log('Registering command', 'run-javascript.run-script');
+  const disposable = vscode.commands.registerCommand(
+    'run-javascript.run-script',
+    () => {
+      const quickPick = vscode.window.createQuickPick();
+      quickPick.items = Object.keys(commands).map((label) => ({ label }));
+      quickPick.onDidChangeSelection((selection) => {
+        if (selection[0]) {
+          commands[selection[0].label](vscode, context)
+            .catch(console.error)
+            .finally(() => quickPick.dispose());
+        }
+      });
+      quickPick.onDidHide(() => quickPick.dispose());
+      quickPick.onDidAccept(() => quickPick.dispose());
+      quickPick.show();
+    }
+  );
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
