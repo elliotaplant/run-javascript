50258a2730ba8160d918ee032871aa6eb99bf19e
Elliot Plant <elliotplant@gmail.com>
2022-08-19 15:45:11 -0700

    Make it work in workspaces and globally

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const vscode = require('vscode');
-const commands = require('/Users/elliot/.vscode-run-javascript/index.js');
 const { copyExampleIfNecessary } = require('./copying');
 
 /**
@@ -8,17 +7,52 @@ const { copyExampleIfNecessary } = require('./copying');
  */
 function activate(context) {
   copyExampleIfNecessary(context.globalStorageUri, 'global');
-  registerRunScripts(context);
-  registerEditScripts(context);
-  registerCustomCommands(context);
+  registerEditScripts(context, context.globalStorageUri, 'global');
+
+  try {
+    registerRunScripts(context);
+    registerCustomCommands(context, context.globalStorageUri);
+  } catch (error) {
+    console.error(error);
+    vscode.window.showErrorMessage(
+      'run-javascript: Unable to load global custom scripts. See console for details.'
+    );
+  }
+
+  if (context.storageUri) {
+    copyExampleIfNecessary(context.storageUri, 'workspace');
+    try {
+      registerEditScripts(context, context.storageUri, 'workspace');
+      registerCustomCommands(context, context.storageUri);
+    } catch (error) {
+      console.error(error);
+      vscode.window.showErrorMessage(
+        'run-javascript: Unable to load workspace custom scripts. See console for details.'
+      );
+    }
+  }
 }
 
 /**
  * @param {vscode.ExtensionContext} context
+ * @param {vscode.Uri} uri
  */
 function registerRunScripts(context) {
   // Register the "run-script" command that can be accessed from the command palette
   console.log('Registering command', 'run-javascript.run-script');
+  const globalCommands = require(path.join(
+    context.globalStorageUri.fsPath,
+    'index.js'
+  ));
+  let workspaceCommands = {};
+  if (context.storageUri) {
+    workspaceCommands = require(path.join(
+      context.storageUri.fsPath,
+      'index.js'
+    ));
+  }
+  const commands = Object.assign({}, globalCommands, workspaceCommands);
+
   const disposable = vscode.commands.registerCommand(
     'run-javascript.run-script',
     () => {
@@ -39,50 +73,40 @@ function registerRunScripts(context) {
   context.subscriptions.push(disposable);
 }
 
-// Register the "edit-global-scripts" and "edit-local-scripts" command to enable editing scripts
+// Register the "edit-global-scripts" and "edit-workspace-scripts" command to enable editing scripts
 /**
  * @param {vscode.ExtensionContext} context
+ * @param {vscode.Uri} uri
+ * @param {'workspace' | 'global'} workspaceOrGlobal
  */
-function registerEditScripts(context) {
-  const globalCommand = 'run-javascript.edit-global-scripts';
-  console.log('Registering command', globalCommand);
-  const globalDisposable = vscode.commands.registerCommand(
-    globalCommand,
-    async () => {
-      vscode.workspace.updateWorkspaceFolders(0, 0, {
-        uri: context.globalStorageUri,
-      });
+function registerEditScripts(context, uri, workspaceOrGlobal) {
+  const commandName = `run-javascript.edit-${workspaceOrGlobal}-scripts`;
+  console.log('Registering command', commandName);
+  const disposable = vscode.commands.registerCommand(commandName, async () => {
+    vscode.workspace.updateWorkspaceFolders(0, 0, {
+      uri: uri,
+    });
 
-      await focusIndex(context.globalStorageUri);
-    }
-  );
-  context.subscriptions.push(globalDisposable);
-
-  const localCommand = 'run-javascript.edit-local-scripts';
-  console.log('Registering command', localCommand);
-  const localDisposable = vscode.commands.registerCommand(
-    localCommand,
-    async () => {
-      copyExampleIfNecessary(context.storageUri, 'workspace');
-      await vscode.workspace.updateWorkspaceFolders(0, 0, {
-        uri: context.storageUri,
-      });
-      await focusIndex(context.storageUri);
-    }
-  );
-  context.subscriptions.push(localDisposable);
+    await focusIndex(uri);
+  });
+  context.subscriptions.push(disposable);
 }
 
 // Register commands for all of the methods in the commands directory
-function registerCustomCommands(context) {
+async function registerCustomCommands(context, uri) {
   console.log('Registering custom commands for "run-javascript"');
+  const commands = require(path.join(uri.fsPath, 'index.js'));
+  const allCommands = new Set(await vscode.commands.getCommands());
   Object.entries(commands).forEach(([functionName, commandFunction]) => {
     const commandName = `run-javascript.${functionName}`;
-    console.log('Registering command', commandName);
-    const disposable = vscode.commands.registerCommand(commandName, (...args) =>
-      commandFunction(vscode, context, ...args)
-    );
-    context.subscriptions.push(disposable);
+    if (!allCommands.has(commandName)) {
+      console.log('Registering commands', commandName);
+      const disposable = vscode.commands.registerCommand(
+        commandName,
+        (...args) => commandFunction(vscode, context, ...args)
+      );
+      context.subscriptions.push(disposable);
+    }
   });
 }
 
